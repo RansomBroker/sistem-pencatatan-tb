78dedaa44e5dd92b73a0972a3850a9abf0cecc4a
RansomBroker <yadisoke@gmail.com>
2024-02-08 03:28:19 +0700

    perbaiki migration files

---
+++ database/migrations/2024_02_08_031840_update_advance_receive.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new  class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('advance_receives', function (Blueprint $table) {
+            $table->decimal('buy_price', 16, 9)->nullable()->change();
+            $table->decimal('net_sale', 16, 9)->nullable()->change();
+            $table->decimal('tax', 16, 9)->nullable()->change();
+            $table->decimal('unit_price', 16, 9)->nullable()->change();
+            $table->decimal('idr_total', 16, 9)->nullable()->change();
+            $table->decimal('idr_expired', 16, 9)->nullable()->change();
+            $table->decimal('idr_refund', 16, 9)->nullable()->change();
+            $table->decimal('idr_sum_all', 16, 9)->nullable()->change();
+            $table->decimal('idr_remains', 16, 9)->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('advance_receives', function (Blueprint $table) {
+            $table->decimal('buy_price', 16, 4)->nullable()->change();
+            $table->decimal('net_sale', 16, 4)->nullable()->change();
+            $table->decimal('tax', 16, 4)->nullable()->change();
+            $table->decimal('unit_price', 16, 4)->nullable()->change();
+            $table->decimal('idr_total', 16, 4)->nullable()->change();
+            $table->decimal('idr_expired', 16, 4)->nullable()->change();
+            $table->decimal('idr_refund', 16, 4)->nullable()->change();
+            $table->decimal('idr_sum_all', 16, 4)->nullable()->change();
+            $table->decimal('idr_remains', 16, 4)->nullable()->change();
+        });
+    }
+};
